e22fc64ae2f8b30d9f75d9a1f580f540a41d197f
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-03-15 12:17:30 -0300

    connect API

--- cost/src/components/project/ProjectForm.js
+++ cost/src/components/project/ProjectForm.js
@@ -1,9 +1,26 @@
+import {useEffect, useState} from 'react'
+
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButtom'
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({btnText}){
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+      }, [])
 
     return(
         <form className={styles.form}>
@@ -19,7 +36,7 @@ function ProjectForm({btnText}){
                 name="budget"
                 placeholder="Insira o orçamento total"
             />
-            <Select name="category_id" text="Selecione a categoria:" />
+            <Select name="category_id" text="Selecione a categoria:" options={categories}/>
             <SubmitButton text={btnText} />
         </form>
     )
